ec15b4d6a65edbabc32113c0751fe0ae0dbd9fb4
Tommy Hagberg <tommy.hagberg@vgregion.se>
2024-02-13 13:30:14 +0100

    ny

---
+++ Unlock/Unlock/Datacontainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unlock
+{
+    class Datacontainer
+    {
+        public static string connectionString;
+        public static string anvandarnamn;
+        public static string losen;
+        public static string connectsource;
+        public static string personnummer;
+        public static string Familyname;
+        public static string fornamn;
+        public static SqlConnection cnn;
+        public static SqlCommand command, command2;
+        public static int Index;
+    }
+}
--- Unlock/Unlock/Form1.cs
+++ Unlock/Unlock/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,10 +16,34 @@ namespace Unlock
         public Form1()
         {
             InitializeComponent();
+            InitializeComponent();
+            Datacontainer.connectsource = "Data Source=Klingen-su-db,62468; Initial Catalog = Klingen;";
         }
 
         private void label4_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Datacontainer.anvandarnamn = textBox1.Text;
+            Datacontainer.losen = textBox2.Text;
+            Datacontainer.connectionString = @Datacontainer.connectsource + "User ID=" + textBox1.Text + ";Password=" + textBox2.Text + "";
+            Datacontainer.cnn = new SqlConnection(Datacontainer.connectionString);
+            Datacontainer.cnn.Open();
+            string message = "Connection Open  !";
+            string title = "";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.OK)
+            {
+                button2.Enabled = true;
+            }
+            else
+            {
+                // Do something
+            }
 
         }
     }
