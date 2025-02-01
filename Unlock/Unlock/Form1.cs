2bfe25959f32a5a2b1d45be173dd95327656e9d9
torsus <crosis767@gmail.com>
2025-02-01 08:46:27 +0100

    ny

--- Unlock/Unlock/Form1.cs
+++ Unlock/Unlock/Form1.cs
@@ -63,12 +63,12 @@ namespace Unlock
            // String Sql;
             if (Datacontainer.connectsource == "Data Source=Klingen-su-db,62468; Initial Catalog = Klingen;")
             {
-                Datacontainer.SQLSearch = "SELECT ROW_NUMBER() OVER(ORDER BY[Index] ) AS RowNumber,[Index],Patient,[Analysis Number],AnswerDate,Comments FROM[Klingen].[dbo].[Analysis Answer] WHERE AnswerDate > '" + theDate1 +"'AND Answerdate < '" + theDate2 +"' AND Comments is not null";
+                Datacontainer.SQLSearch = "SELECT ROW_NUMBER() OVER(ORDER BY[Index] ) AS RowNumber,[Index],Patient,[Analysis Number],AnswerDate,Comments FROM[Klingen].[dbo].[Analysis Answer] WHERE AnswerDate >= '" + theDate1 +"'AND Answerdate <= '" + theDate2 +"' AND Comments is not null";
 
             }
             else
             {
-                Datacontainer.SQLSearch = "SELECT ROW_NUMBER() OVER(ORDER BY[Index] ) AS RowNumber,[Index],Patient,[Analysis Number],AnswerDate,Comments FROM[Klingen_test].[dbo].[Analysis Answer] WHERE AnswerDate > '" + theDate1 + "'AND Answerdate < '" + theDate2 + "' AND Comments is not null";
+                Datacontainer.SQLSearch = "SELECT ROW_NUMBER() OVER(ORDER BY[Index] ) AS RowNumber,[Index],Patient,[Analysis Number],AnswerDate,Comments FROM[Klingen_test].[dbo].[Analysis Answer] WHERE AnswerDate >= '" + theDate1 + "'AND Answerdate <= '" + theDate2 + "' AND Comments is not null";
 
             }
             Datacontainer.command = new SqlCommand(Datacontainer.SQLSearch, Datacontainer.cnn);
